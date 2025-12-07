cc44eec5bd0f76592c9106908deac904d678858a
Nidhin MK <nidhinmk@gmail.com>
2025-12-06 20:48:02 -0800

    Added swagger

--- section2/accounts/src/main/java/com/nidhin/accounts/dto/AccountsDto.java
+++ section2/accounts/src/main/java/com/nidhin/accounts/dto/AccountsDto.java
@@ -1,19 +1,33 @@
 package com.nidhin.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
 public class AccountsDto {
 
-    @NotEmpty(message = "Account number cannot be mull or empty")
-    @Pattern(regexp = "(^$|[0-9]{10})", message = "Account number must be 10 digits")
+    @NotEmpty(message = "AccountNumber can not be a null or empty")
+    @Pattern(regexp="(^$|[0-9]{10})",message = "AccountNumber must be 10 digits")
+    @Schema(
+            description = "Account Number of CIBC Bank account", example = "3454433243"
+    )
     private Long accountNumber;
 
-    @NotEmpty(message = "Account Type cannot be mull or empty")
+    @NotEmpty(message = "AccountType can not be a null or empty")
+    @Schema(
+            description = "Account type of CIBC Bank account", example = "Savings"
+    )
     private String accountType;
 
-    @NotEmpty(message = "Branch Address number cannot be mull or empty")
+    @NotEmpty(message = "BranchAddress can not be a null or empty")
+    @Schema(
+            description = "CIBC Bank branch address", example = "123 NewYork"
+    )
     private String branchAddress;
 }
